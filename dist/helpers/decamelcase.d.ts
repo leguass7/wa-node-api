33478856292db014464303f5952c97699ce579ce
leguass7 <leandro.sbrissa@hotmail.com>
2021-11-13 13:44:13 -0300

    test: build

--- dist/helpers/decamelcase.d.ts
+++ dist/helpers/decamelcase.d.ts
@@ -1,2 +1,2 @@
 export declare function decamelcaseStr(text: string, separator?: string): string;
-export default function decamelcase<T>(input: any): T;
+export declare function decamelcase<T>(input: any): T;
